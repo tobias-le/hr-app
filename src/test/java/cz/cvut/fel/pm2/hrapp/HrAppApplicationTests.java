450822a653effbebea55a2c3a5182c9cb30e9081
Tobias Le <letobias@fel.cvut.cz>
2024-10-02 17:33:48 +0200

    ïnitial

---
+++ src/test/java/cz/cvut/fel/pm2/hrapp/HrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.pm2.hrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
