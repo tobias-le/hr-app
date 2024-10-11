3b65a8bdc1222f42aa8144ffbe8b28d1f2ed8d3c
Andrei Zhgulev <zhguland@fel.cvut.cz>
2024-10-11 20:36:07 +0200

    new commit

---
+++ src/main/java/cz/cvut/fel/pm2/hrapp/employeemanagement/model/Employee.java
@@ -0,0 +1,72 @@
+package cz.cvut.fel.pm2.hrapp.employeemanagement.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String position;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private Employee manager;
+
+    @OneToMany(mappedBy = "manager")
+    private List<Employee> subordinates;
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
---
+++ src/main/java/cz/cvut/fel/pm2/hrapp/holidaymanagement/model/HolidayRequest.java
@@ -0,0 +1,73 @@
+package cz.cvut.fel.pm2.hrapp.holidaymanagement.model;
+
+import cz.cvut.fel.pm2.hrapp.employeemanagement.model.Employee;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class HolidayRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String status;
+    private String reason;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public HolidayRequest() {
+    }
+}
