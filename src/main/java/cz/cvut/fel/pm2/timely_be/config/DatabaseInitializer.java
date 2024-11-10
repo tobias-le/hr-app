0fcf1a5b04182065049c9b12a0c4f4678a22642b
Jakub Skříšovský <skrisjak@fel.cvut.cz>
2024-11-10 20:32:52 +0100

    resolved problems with adding initial system data

--- src/main/java/cz/cvut/fel/pm2/timely_be/config/DatabaseInitializer.java
+++ src/main/java/cz/cvut/fel/pm2/timely_be/config/DatabaseInitializer.java
@@ -20,124 +20,126 @@ public class DatabaseInitializer {
 
     @Bean
     CommandLineRunner initEmployeeDatabase(EmployeeRepository employeeRepository, TeamRepository teamRepository
-    , AttendanceRecordRepository attendanceRecordRepository, ProjectRepository projectRepository, EmployeeLeaveBalanceRepository leaveBalanceRepository, LeaveRepository leaveRepository) {
-        return args -> {
-            Project project1 = new Project();
-            project1.setName("Project Alpha");
-            Project project2 = new Project();
-            project2.setName("Project Beta");
-            Project project3 = new Project();
-            project3.setName("Project Gamma");
-            Project project4 = new Project();
-            project4.setName("Project Delta");
-            Project project5 = new Project();
-            project5.setName("Project Epsilon");
+            , AttendanceRecordRepository attendanceRecordRepository, ProjectRepository projectRepository, LeaveRepository leaveRepository, EmployeeLeaveBalanceRepository leaveBalanceRepository) {
+        return args -> resetDatabaseToBaseState(employeeRepository, teamRepository, attendanceRecordRepository, projectRepository, leaveRepository, leaveBalanceRepository);
+    }
 
-            List<Project> projects = Arrays.asList(project1, project2, project3, project4, project5);
-            projectRepository.saveAll(projects);
+    public static void resetDatabaseToBaseState(EmployeeRepository employeeRepository, TeamRepository teamRepository, AttendanceRecordRepository attendanceRecordRepository, ProjectRepository projectRepository, LeaveRepository leaveRepository, EmployeeLeaveBalanceRepository leaveBalanceRepository) {
+        Project project1 = new Project();
+        project1.setName("Project Alpha");
+        Project project2 = new Project();
+        project2.setName("Project Beta");
+        Project project3 = new Project();
+        project3.setName("Project Gamma");
+        Project project4 = new Project();
+        project4.setName("Project Delta");
+        Project project5 = new Project();
+        project5.setName("Project Epsilon");
 
-            // Sample Employee data
-            Employee employee1 = new Employee();
-            employee1.setName("John Doe");
-            employee1.setJobTitle("Software Developer");
-            employee1.setEmploymentStatus(EmploymentStatus.FULL_TIME);
-            employee1.setEmail("john.doe@example.com");
-            employee1.setPhoneNumber("123-456-7890");
-            employee1.setCurrentProjects(List.of(project1, project2));
+        List<Project> projects = Arrays.asList(project1, project2, project3, project4, project5);
+        projectRepository.saveAll(projects);
 
-            Employee employee2 = new Employee();
-            employee2.setName("Jane Smith");
-            employee2.setJobTitle("Project Manager");
-            employee2.setEmploymentStatus(EmploymentStatus.PART_TIME);
-            employee2.setEmail("jane.smith@example.com");
-            employee2.setPhoneNumber("098-765-4321");
-            employee2.setCurrentProjects(List.of(project3, project4));
+        // Sample Employee data
+        Employee employee1 = new Employee();
+        employee1.setName("John Doe");
+        employee1.setJobTitle("Software Developer");
+        employee1.setEmploymentStatus(EmploymentStatus.FULL_TIME);
+        employee1.setEmail("john.doe@example.com");
+        employee1.setPhoneNumber("123456789");
+        employee1.setCurrentProjects(List.of(project1, project2));
 
-            Employee employee3 = new Employee();
-            employee3.setName("Alice Johnson");
-            employee3.setJobTitle("UX Designer");
-            employee3.setEmploymentStatus(EmploymentStatus.CONTRACT);
-            employee3.setEmail("alice.johnson@example.com");
-            employee3.setPhoneNumber("555-123-4567");
-            employee3.setCurrentProjects(List.of(project5));
+        Employee employee2 = new Employee();
+        employee2.setName("Jane Smith");
+        employee2.setJobTitle("Project Manager");
+        employee2.setEmploymentStatus(EmploymentStatus.PART_TIME);
+        employee2.setEmail("jane.smith@example.com");
+        employee2.setPhoneNumber("123456789");
+        employee2.setCurrentProjects(List.of(project3, project4));
 
-            // Save employees to the database
-            List<Employee> allEmployees = Arrays.asList(employee1, employee2, employee3);
-            List<Employee> employees = employeeRepository.saveAll(allEmployees);
+        Employee employee3 = new Employee();
+        employee3.setName("Alice Johnson");
+        employee3.setJobTitle("UX Designer");
+        employee3.setEmploymentStatus(EmploymentStatus.CONTRACT);
+        employee3.setEmail("alice.johnson@example.com");
+        employee3.setPhoneNumber("123456789");
+        employee3.setCurrentProjects(List.of(project5));
 
-            // Sample Team data
-            Team team = new Team();
-            team.setName("Product Team 1");
-            team.setManager(employee2); // Jane Smith as the manager
-            team.setMembers(employees);
+        // Save employees to the database
+        List<Employee> allEmployees = Arrays.asList(employee1, employee2, employee3);
+        List<Employee> employees = employeeRepository.saveAll(allEmployees);
 
-            // Sample Team data
-            Team team2 = new Team();
-            team2.setName("Product Team 2");
-            team2.setManager(employee3);
+        // Sample Team data
+        Team team = new Team();
+        team.setName("Product Team 1");
+        team.setManager(employee2); // Jane Smith as the manager
+        team.setMembers(employees);
 
-            // Sample Team data
-            Team team3 = new Team();
-            team3.setName("Product Team 3");
-            team3.setManager(employee1);
+        // Sample Team data
+        Team team2 = new Team();
+        team2.setName("Product Team 2");
+        team2.setManager(employee3);
 
-            // Save team to the database
-            teamRepository.saveAll(List.of(team, team2, team3));
+        // Sample Team data
+        Team team3 = new Team();
+        team3.setName("Product Team 3");
+        team3.setManager(employee1);
 
-            allEmployees.forEach(employee -> employee.setTeam(team));
-            employeeRepository.saveAll(allEmployees);
+        // Save team to the database
+        teamRepository.saveAll(List.of(team, team2, team3));
 
-            AttendanceRecord record1 = new AttendanceRecord();
-            record1.setMember(employees.get(0)); // John Doe
-            record1.setDate(LocalDate.now());
-            record1.setClockInTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(9, 0)));
-            record1.setClockOutTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(17, 0)));
-            record1.setProject(project1);
+        allEmployees.forEach(employee -> employee.setTeam(team));
+        employeeRepository.saveAll(allEmployees);
 
-            AttendanceRecord record2 = new AttendanceRecord();
-            record2.setMember(employees.get(1)); // Jane Smith
-            record2.setDate(LocalDate.now());
-            record2.setClockInTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(9, 30)));
-            record2.setClockOutTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(16, 30)));
-            record2.setProject(project2);
+        AttendanceRecord record1 = new AttendanceRecord();
+        record1.setMember(employees.get(0)); // John Doe
+        record1.setDate(LocalDate.now());
+        record1.setClockInTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(9, 0)));
+        record1.setClockOutTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(17, 0)));
+        record1.setProject(project1);
 
-            AttendanceRecord record3 = new AttendanceRecord();
-            record3.setMember(employees.get(2)); // Alice Johnson
-            record3.setDate(LocalDate.now());
-            record3.setClockInTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 0)));
-            record3.setClockOutTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(15, 0)));
-            record3.setProject(project3);
-            // Save attendance records to the database
-            attendanceRecordRepository.saveAll(Arrays.asList(record1, record2, record3));
+        AttendanceRecord record2 = new AttendanceRecord();
+        record2.setMember(employees.get(1)); // Jane Smith
+        record2.setDate(LocalDate.now());
+        record2.setClockInTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(9, 30)));
+        record2.setClockOutTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(16, 30)));
+        record2.setProject(project2);
 
-            //give testing employees their balances
-            EmployeeLeaveBalance balance1 = new EmployeeLeaveBalance();
-            balance1.setPersonalDaysLeft(10);
-            balance1.setVacationDaysLeft(20);
-            balance1.setSickDaysLeft(2);
-            balance1.setEmployeeId(employees.get(0).getEmployeeId());
-            EmployeeLeaveBalance balance2 = new EmployeeLeaveBalance();
-            balance2.setPersonalDaysLeft(10);
-            balance2.setVacationDaysLeft(20);
-            balance2.setSickDaysLeft(2);
-            balance2.setEmployeeId(employees.get(1).getEmployeeId());
-            EmployeeLeaveBalance balance3 = new EmployeeLeaveBalance();
-            balance3.setPersonalDaysLeft(10);
-            balance3.setVacationDaysLeft(20);
-            balance3.setSickDaysLeft(2);
-            balance3.setEmployeeId(employees.get(2).getEmployeeId());
-            leaveBalanceRepository.saveAll(List.of(balance1, balance2, balance3));
+        AttendanceRecord record3 = new AttendanceRecord();
+        record3.setMember(employees.get(2)); // Alice Johnson
+        record3.setDate(LocalDate.now());
+        record3.setClockInTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 0)));
+        record3.setClockOutTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(15, 0)));
+        record3.setProject(project3);
+        // Save attendance records to the database
+        attendanceRecordRepository.saveAll(Arrays.asList(record1, record2, record3));
 
-            //give the first employee some new request
-            Leave leave = new Leave();
-            leave.setReason("Bali");
-            leave.setEmployeeId(employees.get(0).getEmployeeId());
-            leave.setLeaveAmount(8);
-            leave.setStartDate(LocalDate.of(2024,11,15));
-            leave.setEndDate(LocalDate.of(2024,11,16));
-            leave.setLeaveType(LeaveType.PERSONAL_LEAVE);
-            leave.setStatus(LeaveStatus.PENDING);
-            leaveRepository.save(leave);
-        };
+        //give testing employees their balances
+        EmployeeLeaveBalance balance1 = new EmployeeLeaveBalance();
+        balance1.setPersonalDaysLeft(10);
+        balance1.setVacationDaysLeft(20);
+        balance1.setSickDaysLeft(2);
+        balance1.setEmployeeId(employees.get(0).getEmployeeId());
+        EmployeeLeaveBalance balance2 = new EmployeeLeaveBalance();
+        balance2.setPersonalDaysLeft(10);
+        balance2.setVacationDaysLeft(20);
+        balance2.setSickDaysLeft(2);
+        balance2.setEmployeeId(employees.get(1).getEmployeeId());
+        EmployeeLeaveBalance balance3 = new EmployeeLeaveBalance();
+        balance3.setPersonalDaysLeft(10);
+        balance3.setVacationDaysLeft(20);
+        balance3.setSickDaysLeft(2);
+        balance3.setEmployeeId(employees.get(2).getEmployeeId());
+        leaveBalanceRepository.saveAll(List.of(balance1, balance2, balance3));
+
+        //give the first employee some new request
+        Leave leave = new Leave();
+        leave.setReason("Bali");
+        leave.setEmployeeId(employees.get(0).getEmployeeId());
+        leave.setLeaveAmount(8);
+        leave.setStartDate(LocalDate.of(2024,11,15));
+        leave.setEndDate(LocalDate.of(2024,11,16));
+        leave.setLeaveType(LeaveType.PERSONAL_LEAVE);
+        leave.setStatus(LeaveStatus.PENDING);
+        leaveRepository.save(leave);
     }
 }
